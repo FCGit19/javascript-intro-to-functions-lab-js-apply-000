3f37c41f4ef6327041febd52891ad745e4f705b6
Frederick Cano <canofred@gmail.com>
2020-03-17 03:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  var lower = string.toLowerCase();
+  var upper = string.toUpperCase();
+  
+  if (lower) {
     return "I can\'t hear you!";
   }
-  else if (string.toUpperCase()) {
+  else if (upper) {
     return "YES INDEED" + "!";
   }
   else if (string === "I love you, Grandma.") {
